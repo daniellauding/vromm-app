d1a0d47739152d47fa82de2263b7e3dd4bccae1d
daniellauding <daniel@lauding.se>
2025-05-13 10:38:45 +0200

    added support for dark and light mode

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,3 +1,7 @@
 import { config } from './src/theme'
 
-export default config 
+// Make sure the config is properly exported
+const tamaguiConfig = config
+
+export default tamaguiConfig
+export type Conf = typeof tamaguiConfig 
