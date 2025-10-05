fea859c79cb1f203d96180022b252cc67ec3b65a
daniellauding <daniel@lauding.se>
2025-10-05 22:20:19 +0200

    fix

--- ADD_COLLECTION_INVITATION_NOTIFICATION_TYPE.sql
+++
@@ -1,8 +0,0 @@
--- Add collection_invitation to notification_type enum
--- This allows collection sharing invitations to create proper notifications
-
--- Add the new enum value
-ALTER TYPE notification_type ADD VALUE 'collection_invitation';
-
--- Note: You may need to restart your application or reconnect to the database
--- for the new enum value to be available in your application code.
--- VERIFY_NOTIFICATION_TYPES.sql
+++
@@ -1,5 +0,0 @@
--- Verify that collection_invitation was added to notification_type enum
--- Run this after executing ADD_COLLECTION_INVITATION_NOTIFICATION_TYPE.sql
-
-SELECT unnest(enum_range(NULL::notification_type)) as notification_types
-ORDER BY notification_types;
